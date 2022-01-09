077b90b4290e6757f1d5d33f9e08f21c3c0f2972
AmanKamani <kamaniamanak@gmail.com>
2022-01-09 23:28:00 +0530

    Use of Customized Material Theme.
    - By default Material Theme uses Baseline Theme (Purple Color).
    - Created custom Color [dark & light], Typography, Shape.
    - Use of Material resources instead of static resources (ex.colors).
    - Changed Home.kt based on the customized material resources & styles.

---
+++ ComposeTheming/app/src/main/java/jb/production/composetheming/ui/theme/Shape.kt
@@ -0,0 +1,12 @@
+package jb.production.composetheming.ui.theme
+
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val JetpackNewsShape = Shapes(
+    small = CutCornerShape(topStart = 8.dp),
+    medium = CutCornerShape(topStart = 24.dp),
+    large = RoundedCornerShape(8.dp)
+)
