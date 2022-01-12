44ac5208b0ea150cd1bc02acd44059c6ad26853f
AmanKamani <kamaniamanak@gmail.com>
2022-01-12 17:33:17 +0530

    Compose Testing with Rally App
    Rally is an existing project of compose-codelabs.
    - Created test cases to test the existence of element
    - Use of unmergedTree
    - Debugging & Logging of Test Cases.

---
+++ ComposeTesting/app/src/androidTest/java/com/example/compose/rally/OverviewScreenTest.kt
@@ -0,0 +1,26 @@
+package com.example.compose.rally
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import com.example.compose.rally.ui.overview.OverviewBody
+import org.junit.Rule
+import org.junit.Test
+
+class OverviewScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    // shows the test case for infinite animation (Synchronization)
+    @Test
+    fun overviewScreen_alertsDisplayed() {
+        composeTestRule.setContent {
+            OverviewBody()
+        }
+
+        composeTestRule
+            .onNodeWithText("Alerts")
+            .assertIsDisplayed()
+    }
+}
